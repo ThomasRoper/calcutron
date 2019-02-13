918337fa231389839873fab08400195fec822595
thomas <thomas@localhost.localdomain>
2019-02-13 19:57:11 +0000

    Calculator now works

--- app/controllers/calculator_controller.rb
+++ app/controllers/calculator_controller.rb
@@ -1,9 +1,15 @@
 class CalculatorController < ApplicationController
   def index
+    puts "Home action says: Hello world!"
+    @operation = '+'
+    @a = 2
+    @b = 3
   end
 
   def new
-    @result = Calculator.send(params[:operation], *[params[:a], params[:b]])
+    unless params[:operation].nil? && params[:a].nil? && params[:a].nil? 
+      @result = Calculator.send(params[:operation], *[params[:a], params[:b]])
+    end
     render :index
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #root to: 'pages#home'
-  root to: 'calculator#index'
+  #root to: 'calculator#index'
+  
+  resource :calculator do
+    # Route GET /user/admin_login
+    get 'products' => 'calculator#new'
+  end
+
 end
